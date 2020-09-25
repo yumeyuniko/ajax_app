17d415bb2050b9ffc3f67ac63d66e9db424ccb07
yumeyuniko <daigocafe@msn.com>
2020-09-25 18:52:26 +0900

    Revert "既読機能のサーバーサイド実装"
    This reverts commit f886dd0773cf6225c9dc2ac5b49272d3fe3dadc4.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,20 +4,8 @@ class PostsController < ApplicationController
  end
 
 
-  def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
-  end
-
-  def checked
-    post = Post.find(params[:id])
-    if post.checked 
-      post.update(checked: false)
-    else
-      post.update(checked: true)
-    end
-
-    item = Post.find(params[:id])
-    render json: { post: item }
-  end
+def create
+  Post.create(content: params[:content])
+  redirect_to action: :index
+end
 end
