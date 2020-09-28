25f22f5b5026d6152e9af1907265350d0f581d2d
yumeyuniko <daigocafe@msn.com>
2020-09-28 17:36:46 +0900

    commit

--- db/migrate/20200718050723_create_posts.rb
+++ db/migrate/20200718050723_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
-      t.boolean :cheacked
+      t.boolean :checked
       t.timestamps
     end
   end
